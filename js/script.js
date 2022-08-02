d8629de4b792bd380a5eca270c744b2c4fb3a451
eiki-ogawa <eiki-ogawa@x-power.co.jp>
2022-08-02 18:03:23 +0900

    クリックで駒を置く

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 class Othello {
     constructor() {
+        // オセロ盤の要素（table）
         this.board_element = document.getElementById("board");
+        // 手番
+        this.whitch_turn = "black";
     }
 
+    // index.html から呼ばれる
     startGame() {
         this.createBoard()
+        // 初期配置
+        this.put_piece(3,3)
+        this.put_piece(3,4)
+        this.put_piece(4,4)
+        this.put_piece(4,3)
+
+        // 盤面がクリックされたら駒を置く
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                document.getElementById(i + " " + j).addEventListener(
+                    "click", (e) => this.put_piece(i, j), false
+                    );
+            }
+        }
     }
 
+    //ボードをセットする
     createBoard() {
-
-        for (let i = 1; i <= 8; i++) {
+        for (let i = 0; i < 8; i++) {
             var tr = document.createElement("tr");
-            for (let j = 1; j <= 8; j ++) {
+            for (let j = 0; j < 8; j ++) {
                 var td = document.createElement("td");
                 var div = document.createElement("div");
                 if (i == 3 && j == 3) {
@@ -20,10 +38,28 @@ class Othello {
                 if (i == 3 && j == 4) {
                     div.className = "piece white";
                 }
+                // idに座標を入力
+                div.id = i + " " + j
                 td.appendChild(div);
                 tr.appendChild(td);
             }
             this.board_element.appendChild(tr);
+        }
+    }
+
+    // 駒を置く
+    put_piece(x, y) {
+        var target_elem = document.getElementById(x + " " + y);
+        target_elem.className = "piece " + this.whitch_turn;
+        this.change_turn()
+    }
+
+    // 手番を変える
+    change_turn() {
+        if(this.whitch_turn == "black") {
+            this.whitch_turn = "white";
+        } else {
+            this.whitch_turn = "black";
         }
     }
 }
